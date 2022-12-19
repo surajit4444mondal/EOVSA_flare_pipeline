92e7a59df559b0b3b8357f0364e1a1688506fb2a
Surajit Mondal <surajit4444mondal@gmail.com>
2022-12-19 18:05:44 -0500

    the gen_IDB_MS.py was completely wrong. Updating the correct version

--- gen_IDB_MS.py
+++ gen_IDB_MS.py
@@ -1,6 +1,17 @@
-from inputs import *
+from suncasa.tasks import task_calibeovsa as calibeovsa
+from suncasa.tasks import task_importeovsa as timporteovsa
+from split_cli import split_cli as split
+from util import Time
+import dump_tsys
+import numpy as np
 import os
+import glob
+from eovsapy import util
+import datetime as dt
+import subprocess
 
+os.system("rm -rf inputs.pyc")
+from inputs import *
 
 os.chdir(workpath)
 workdir = os.getcwd() + '/'  # main working directory. Using current directory in this example
@@ -21,9 +32,7 @@ else:
     raise RuntimeError("Ordering ot times wrong")
 
 namesuffix = '_' + trange[0].to_datetime().strftime('%H%M') + '-' + trange[1].to_datetime().strftime('%H%M')
-
 idbdir = util.get_idbdir(trange[0])
-
 info = dump_tsys.rd_fdb(Time(starttime.split(' ')[0]))
 idxs, = np.where(info['SOURCEID'] == '')
 for k, v in info.iteritems():
@@ -32,23 +41,23 @@ sidx = np.where(
     np.logical_and(info['SOURCEID'] == 'Sun', info['PROJECTID'] == 'NormalObserving') & np.logical_and(
         info['ST_TS'].astype(np.float) >= trange[0].lv,
         info['ST_TS'].astype(np.float) <= trange[1].lv))
-filelist = info['FILE'][sidx]
 
+filelist = info['FILE'][sidx]
 
 outpath = 'msdata/'
 if not os.path.exists(outpath):
     os.makedirs(outpath)
 inpath = idbdir + '{}/'.format(trange[0].datetime.strftime("%Y%m%d"))
 ncpu = 1
-# TODO How can we stop importeovsa from repeatedly creaing the same MS.
+
 print ("Starting to import IDB MS")
 msfiles = timporteovsa.importeovsa(idbfiles=[inpath + ll for ll in filelist], ncpu=ncpu, timebin="0s", width=1,\
                                  visprefix=outpath, nocreatms=False, doconcat=False,\
                               modelms="", doscaling=False, keep_nsclms=False, udb_corr=True,\
                             use_exist_udbcorr=True)
 print ("IDB MS imported")
-#msfiles = [outpath + ll + '.ms' for ll in filelist]
-
+msfiles = [outpath + ll + '.ms' for ll in filelist]
+print(msfiles)
 concatvis = os.path.basename(msfiles[0])[:11] + namesuffix + '.ms'
 print ("Running calibeovsa, concatting to ", concatvis)
 vis = calibeovsa.calibeovsa(msfiles, caltype=['refpha', 'phacal'], interp='nearest', doflag=True,\
